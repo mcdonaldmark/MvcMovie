aca52872c95e9f52c09402ee57e3d4bbdc3c31fc
mcdonaldmark <markamcdonald7@gmail.com>
2026-05-12 18:24:00 -0600

    sync

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6df1400ceda3acda8ebbd9792f91e2d8f0cb870")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32583dcabded9bf12d26fc1a3563586f6f2f119")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
